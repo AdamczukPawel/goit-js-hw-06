c06ae02683b6e643c0643c8838dbb1fe2a1c7bfe
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-09-25 17:18:28 +0200

    Ukończone zadanie 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,18 +1,10 @@
 const input = document.querySelector(`#name-input`);
 const output = document.querySelector(`#name-output`);
-console.log(input.value);
 
-// if (input.value !== null) {
-//     input.addEventListener("input", (event) => {
-//     output.textContent = event.currentTarget.value;
-//     })
-// } else {
-//     output.textContent = "Anonymous";
-// };
 input.addEventListener("input", (event) => {
-    if (input.textContent !== null) {
-        output.textContent = event.currentTarget.value;
-    } else {
+    if (input.value === "") {
         output.textContent = "Anonymous";
+    } else {
+        output.textContent = event.currentTarget.value;
     }
 });
