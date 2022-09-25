88a581b0483c6bf24254dbbe9fe53d6d83ddf651
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-09-25 19:28:46 +0200

    Ukończenie zadania 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+    
+    if (email.value !== "" && password.value !== "") {
+        console.log(`Email: ${email.value}, Password: ${password.value}`);
+        form.reset();
+    } else {
+        window.alert("All fields must be filled");
+    }
+})
