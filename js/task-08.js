0c748703f6e9f4ee0e4e9b9607731725dc5074e7
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-09-27 21:59:27 +0200

    Poprawka zadania 8

--- js/task-08.js
+++ js/task-08.js
@@ -7,9 +7,10 @@ form.addEventListener("submit", (event) => {
     } = event.currentTarget;
     
     if (email.value !== "" && password.value !== "") {
-        console.log(`Email: ${email.value}, Password: ${password.value}`);
+        console.log(`${email.name}: ${email.value}, ${password.name}: ${password.value}`);
         form.reset();
     } else {
         window.alert("All fields must be filled");
     }
+    console.log(event);
 })
